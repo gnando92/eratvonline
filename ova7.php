247b9378c662a7d5207bc020404df90b7fbc8e60
streamingseru.com <streamingseru.com@gmail.com>
2014-08-22 06:47:56 +0000

    aa

---
+++ ova7.php
@@ -0,0 +1,6 @@
+<config>
+<ova.title>Video Ads - streamingseru.com</ova.title>
+<ova.json>
+{ "debug": { "levels": "fatal, config, vast_template, vpaid, http_calls, playlist, api" }, "ads": { "skipAd": { "enabled": true, "showAfterSeconds": 6 }, "providers": { "enforceSettingSubscribeRTMP":true, "subscribeRTMP": false }, "schedule": [ { "position": "pre-roll", "tag": "http://delivery.ambient-platform.com/247/xmlvideoad/zid_1401690255/wid_1401690137/type_inline/cb_[timestamp]category:LF;api_test:false;format:vast;tagtype:publisher;ru=http://streamingseru.com;rand=4263460588" } ] } }
+</ova.json>
+</config>
