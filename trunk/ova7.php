6213e2191a3d61bd6124baffbb7d91b69da91177
streamingseru.com@gmail.com <streamingseru.com@gmail.com@c88c463c-5877-ce91-d2fd-99a1d5cea2ad>
2014-08-22 07:06:48 +0000

    aa

--- trunk/ova7.php
+++ trunk/ova7.php
@@ -1,6 +1,39 @@
-<config>
-<ova.title>Video Ads - streamingseru.com</ova.title>
-<ova.json>
-{ "debug": { "levels": "fatal, config, vast_template, vpaid, http_calls, playlist, api" }, "ads": { "skipAd": { "enabled": true, "showAfterSeconds": 6 }, "providers": { "enforceSettingSubscribeRTMP":true, "subscribeRTMP": false }, "schedule": [ { "position": "pre-roll", "tag": "http://delivery.ambient-platform.com/247/xmlvideoad/zid_1401690255/wid_1401690137/type_inline/cb_[timestamp]category:LF;api_test:false;format:vast;tagtype:publisher;ru=http://streamingseru.com;rand=4263460588" } ] } }
-</ova.json>
-</config>
+<?php
+
+header('Content-type: text/xml');
+header('Pragma: public');
+header('Cache-control: private');
+header('Expires: -1');
+$random = substr(number_format(time() * rand(),0,'',''),0,10);
+
+echo '<config>
+   <ova.title>
+       Video Ads - streaming.eratvonline.com
+   </ova.title>
+   <ova.json>
+       {
+          "debug": {
+              "levels": "fatal, config, vast_template, vpaid, http_calls, playlist, api"
+          },
+
+          "ads": {
+              "skipAd": {
+                  "enabled": true,
+                  "showAfterSeconds": 6
+              },
+              "providers": {
+                  "enforceSettingSubscribeRTMP":true,
+                  "subscribeRTMP": false
+              },
+              "schedule": [
+                  {
+                      "position": "pre-roll",
+                      "tag": "http://serve.vdopia.com/adserver/vastxml/f65cc920fba56972c31af69c6d0c1936/preroll/category:LF;api_test:false;format:vast;tagtype:publisher;ru=http://www.poztmo.com;rand='.$random.'"
+                 }
+              ]
+          }
+       }
+   </ova.json>
+</config>';
+?>
+
