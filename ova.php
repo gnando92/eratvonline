a3cdd773e158fabff176f5d5ded4950f50e4e652
streamingseru.com <streamingseru.com@gmail.com>
2014-09-01 16:25:15 +0000

    aaa

--- ova.php
+++ ova.php
@@ -1,39 +1,5 @@
-<?php
-
-header('Content-type: text/xml');
-header('Pragma: public');
-header('Cache-control: private');
-header('Expires: -1');
-$random = substr(number_format(time() * rand(),0,'',''),0,10);
-
-echo '<config>
-   <ova.title>
-       Video Ads - streaming.eratvonline.com
-   </ova.title>
-   <ova.json>
-       {
-          "debug": {
-              "levels": "fatal, config, vast_template, vpaid, http_calls, playlist, api"
-          },
-
-          "ads": {
-              "skipAd": {
-                  "enabled": true,
-                  "showAfterSeconds": 6
-              },
-              "providers": {
-                  "enforceSettingSubscribeRTMP":true,
-                  "subscribeRTMP": false
-              },
-              "schedule": [
-                  {
-                      "position": "pre-roll",
-  "tag": "http://delivery.ambient-platform.com/247/xmlvideoad/zid_1401690255/wid_1401690137/type_inline/cb_[timestamp]/category:LF;api_test:false;format:vast;tagtype:publisher;ru=http://www.streamingseru.com;rand='.$random.'"
-                 }
-              ]
-          }
-       }
-   </ova.json>
-</config>';
-?>
-
+<config>
+<innityadsplugin.json>
+{ "ads": { "lightbox":false, "skipAd": { "enabled": true, "showAfterSeconds": 5 }, "schedule":[ { "position":"pre-roll", "tag":"http://delivery.ambient-platform.com/247/xmlvideoad/zid_1401690255/wid_1401690137/type_inline/cb_[timestamp]/category:EN;api_test:false;format:both;tagtype:publisher;ru=http://www.streamingseru.com;rand=$random" } , { "position":"enroll", "tag":"http://serve.vdopia.com/adserver/vastxml/cdbd302099c27a30024cf223351f4223/preroll/category:EN;api_test:false;format:both;tagtype:publisher;ru=http://www.streamingseru.com;rand=$random" } ] } }
+</innityadsplugin.json>
+</config>
