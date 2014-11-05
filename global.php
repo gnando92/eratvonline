041a6f1b374a9bd2dda2acac4f2f3663b5aa81e5
streamingseru.com@gmail.com <streamingseru.com@gmail.com@c88c463c-5877-ce91-d2fd-99a1d5cea2ad>
2014-11-05 10:20:32 +0000

    aaaaaa

---
+++ global.php
@@ -0,0 +1,17 @@
+<meta name="robots" content="noindex" />
+<style>body{margin:0;padding:0}</style>
+<script src="http://startivi.googlecode.com/svn/jwplayer.js" type="text/javascript"></script>
+<div id="tv"></div>
+<script type="text/javascript">
+jwplayer("tv").setup({
+  file: "http://103.18.239.41/8g9x4zwcjns3/smil:globaltv2_all/chunklist_w852731175_b1122304.m3u8",
+        title: 'GLOBAL TV',
+        image:'',
+        width: '100%',
+        height:'100%',
+        stretching: "exactfit",
+        autostart: 'true',
+        abouttext:"TV",
+        aboutlink: "http://www.indostreamingtv.com/",
+  primary: 'flash',
+});</script>
